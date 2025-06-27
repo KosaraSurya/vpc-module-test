287a7fa0d1c45ef15b28a4244e22061af00d4a85
Surya Prakash <suryasrinivas765@gmail.com>
2025-06-28 01:36:45 +0530

    vpc module

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    source = "git::https://github.com/KosaraSurya/terraform-aws-vpc.git?refmain"
+    #source = "../terraform-aws-vpc"
     /* project = "roboshop"
     environment = "dev"
     public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24"] */
